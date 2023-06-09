52f92da8eb49b7095af32a89034ba425b0a59d82
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-09 16:35:14 +0900

    feat: 로그인, 회원가입, 리스트 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { routeConfigs } from "./route";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          {routeConfigs.map(({ path, element }) => (
+            <Route path={path} element={element} />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
