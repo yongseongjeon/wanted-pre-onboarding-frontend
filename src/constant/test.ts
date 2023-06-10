2ac6805c6e507b202a669a58858260dece4b3284
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-10 13:44:24 +0900

    feat: 테스트 아이디 문자열 모듈화

---
+++ src/constant/test.ts
@@ -0,0 +1,19 @@
+const TEST_ID = {
+  INPUT: {
+    EMAIL: "email-input",
+    PASSWORD: "password-input",
+    NEW_TODO: "new-todo-input",
+    MODIFY: "modify-input",
+  },
+  BUTTON: {
+    SIGN_UP: "signup-button",
+    SIGN_IN: "signin-button",
+    NEW_TODO_ADD: "new-todo-add-button",
+    MODIFY: "modify-button",
+    DELETE: "delete-button",
+    SUBMIT: "submit-button",
+    CANCEL: "cancel-button",
+  },
+} as const;
+
+export { TEST_ID };
