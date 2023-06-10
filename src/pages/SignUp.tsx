5883fd1b93e46ad506aea34dd88cd20ad8dde861
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-10 14:06:43 +0900

    feat: 회원가입 폼 추가

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,13 @@
+import { TEST_ID } from "../constant/test";
+
 function SignUp() {
-  return <div>SignUp page</div>;
+  return (
+    <>
+      <input data-testid={TEST_ID.INPUT.EMAIL} type="text" />
+      <input data-testid={TEST_ID.INPUT.PASSWORD} type="password" />
+      <button data-testid={TEST_ID.BUTTON.SIGN_UP}>회원가입</button>
+    </>
+  );
 }
 
 export default SignUp;
