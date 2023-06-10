a4dc060caf06f55656808c4ea6d6df8d32b6c6c0
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-10 15:12:25 +0900

    test: 비밀번호 검증 함수 테스트 작성

--- src/utils/util.test.js
+++ src/utils/util.test.js
@@ -1,4 +1,4 @@
-import { validateEmail } from "./util";
+import { validateEmail, validatePassword } from "./util";
 
 describe("Util", () => {
   it("validateEmail의 이메일에 @가 없으면 false를 반환한다.", () => {
@@ -8,5 +8,13 @@ describe("Util", () => {
   it("validateEmail의 이메일에 @가 있으면 true를 반환한다.", () => {
     const mockEmail = "isjd84@naver.com";
     expect(validateEmail(mockEmail)).toBeTruthy();
+  });
+  it("비밀번호가 8자 미만이면 false를 반환한다.", () => {
+    const mockPassword = "1234567";
+    expect(validatePassword(mockPassword)).toBeFalsy();
+  });
+  it("비밀번호가 8자 이상이면 true를 반환한다.", () => {
+    const mockPassword = "12345678";
+    expect(validatePassword(mockPassword)).toBeTruthy();
   });
 });
