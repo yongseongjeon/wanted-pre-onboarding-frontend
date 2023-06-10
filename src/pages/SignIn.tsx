12ac71ee63ebb6532c4865c7106e63b8766b86b6
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-10 17:06:42 +0900

    feat: 로그인, 회원가입 버튼에 disabled 조건 추가

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,6 @@
 import Input from "../components/Input/Input";
 import { TEST_ID } from "../constants/test";
+import useFormValidation from "../hooks/useFormValidation";
 import useInput from "../hooks/useInput";
 
 const INIT_STATE = "";
@@ -7,12 +8,15 @@ const INIT_STATE = "";
 function SignIn() {
   const [email, handleChangeEmail] = useInput(INIT_STATE);
   const [password, handleChangePassword] = useInput(INIT_STATE);
+  const isValid = useFormValidation({ email, password });
 
   return (
     <>
       <Input type="email" value={email} onChange={handleChangeEmail} testId={TEST_ID.INPUT.EMAIL} />
       <Input type="password" value={password} onChange={handleChangePassword} testId={TEST_ID.INPUT.PASSWORD} />
-      <button data-testid={TEST_ID.BUTTON.SIGN_IN}>로그인</button>
+      <button disabled={!isValid} data-testid={TEST_ID.BUTTON.SIGN_IN}>
+        로그인
+      </button>
     </>
   );
 }
