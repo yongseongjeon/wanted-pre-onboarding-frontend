301e72e6a3c0e575c08e8e97bfe4c76b7d17b89a
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-10 14:04:15 +0900

    feat: 로그인 페이지에 로그인 폼 추가

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,13 @@
+import { TEST_ID } from "../constant/test";
+
 function SignIn() {
-  return <div>SignIn page</div>;
+  return (
+    <>
+      <input data-testid={TEST_ID.INPUT.EMAIL} type="text" />
+      <input data-testid={TEST_ID.INPUT.PASSWORD} type="password" />
+      <button data-testid={TEST_ID.BUTTON.SIGN_IN}>로그인</button>
+    </>
+  );
 }
 
 export default SignIn;
