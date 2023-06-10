99e6da247fc32df42423c91127e5e03e39e6ae82
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-10 17:57:34 +0900

    feat: 회원가입 버튼 클릭 시 회원가입 요청 및 성공하면 로그인 페이지 이동

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,20 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import { signUp } from "../api/request";
 import Input from "../components/Input/Input";
 import { TEST_ID } from "../constants/test";
 import useFormValidation from "../hooks/useFormValidation";
 import useInput from "../hooks/useInput";
+import { ROUTE_PATH } from "../route";
 
 const INIT_STATE = "";
 
 function SignUp() {
   const [email, handleChangeEmail] = useInput(INIT_STATE);
   const [password, handleChangePassword] = useInput(INIT_STATE);
-  const isValid = useFormValidation({ email, password });
+  const isFormValid = useFormValidation({ email, password });
+  const router = useNavigate();
+
+  const handleClickSubmitBtn = async () => {
+    const res = await signUp({ email, password });
+    if (!res.ok) {
+      alert("회원가입에 실패했습니다.");
+    }
+    router(ROUTE_PATH.SIGN_IN);
+  };
 
   return (
     <>
       <Input type="email" value={email} onChange={handleChangeEmail} testId={TEST_ID.INPUT.EMAIL} />
       <Input type="password" value={password} onChange={handleChangePassword} testId={TEST_ID.INPUT.PASSWORD} />
-      <button disabled={!isValid} data-testid={TEST_ID.BUTTON.SIGN_UP}>
+      <button disabled={!isFormValid} onClick={handleClickSubmitBtn} data-testid={TEST_ID.BUTTON.SIGN_UP}>
         회원가입
       </button>
     </>
