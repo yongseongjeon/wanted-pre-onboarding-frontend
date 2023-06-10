b873fbfba3107f69144346fa6b51fb00bd280301
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-10 23:04:57 +0900

    feat: API_URL의 문자열과 함수 분리

--- src/api/url.ts
+++ src/api/url.ts
@@ -5,11 +5,14 @@ const API_URL = {
   SIGN_IN: "/auth/signin",
   CREATE_TODO: "/todos",
   GET_TODO: "/todos",
+} as const;
+
+const GET_API_URL = {
   UPDATE_TODO: (id: number) => `/todos/:${id}`,
   DELETE_TODO: (id: number) => `/todos/:${id}`,
-} as const;
+};
 
 const SIGN_UP_API_URL = END_POINT + API_URL.SIGN_UP;
 const SIGN_IN_API_URL = END_POINT + API_URL.SIGN_IN;
 
-export { SIGN_UP_API_URL, SIGN_IN_API_URL };
+export { API_URL, SIGN_UP_API_URL, SIGN_IN_API_URL };
