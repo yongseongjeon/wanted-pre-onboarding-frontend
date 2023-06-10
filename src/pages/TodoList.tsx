ce5c5172561bea5cbcf4c666c8f594861639e247
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-10 23:05:58 +0900

    feat: 할 일 목록 페이지에서 로그인 여부에 따라 로그인 페이지로 리다이렉트

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,4 +1,18 @@
+import { validateSignIn } from "../utils/validation";
+import { ROUTE_PATH } from "../route";
+import useRouter from "../hooks/useRouter";
+import { useEffect } from "react";
+
 function TodoList() {
+  const { routeTo } = useRouter();
+
+  useEffect(function redirectSignInPageIfNotSignedIn() {
+    const isSignedIn = validateSignIn();
+    if (!isSignedIn) {
+      routeTo(ROUTE_PATH.SIGN_IN);
+    }
+  }, []);
+
   return <div>TodoList page</div>;
 }
 
