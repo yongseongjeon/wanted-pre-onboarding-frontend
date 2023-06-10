b67d3a9c0ca95c7086513f73d9b860bdcfd7c6d5
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-10 14:06:12 +0900

    test: 회원가입 페이지 테스트 생성

---
+++ src/pages/SignUp.test.js
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+import { TEST_ID } from "../constant/test";
+import SignUp from "./SignUp";
+
+describe("SignIn page", () => {
+  it("이메일 입력창을 표시한다.", () => {
+    const { getByTestId } = render(<SignUp />);
+    const emailInput = getByTestId(TEST_ID.INPUT.EMAIL);
+    expect(emailInput).toBeInTheDocument();
+  });
+  it("비밀번호 입력창을 표시한다.", () => {
+    const { getByTestId } = render(<SignUp />);
+    const passwordInput = getByTestId(TEST_ID.INPUT.PASSWORD);
+    expect(passwordInput).toBeInTheDocument();
+  });
+  it("로그인 버튼을 표시한다.", () => {
+    const { container, getByTestId } = render(<SignUp />);
+    const signUpButton = getByTestId(TEST_ID.BUTTON.SIGN_UP);
+    expect(signUpButton).toBeInTheDocument();
+    expect(container).toHaveTextContent("회원가입");
+  });
+});
