05cd4f21cfe184274d9eaad70c17364211902ecd
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-10 15:05:17 +0900

    feat: 로그인, 회원가입 페이지에 <Input />, useInput 활용

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,10 +1,17 @@
-import { TEST_ID } from "../constant/test";
+import Input from "../components/Input/Input";
+import { TEST_ID } from "../constants/test";
+import useInput from "../hooks/useInput";
+
+const INIT_STATE = "";
 
 function SignIn() {
+  const [email, handleChangeEmail] = useInput(INIT_STATE);
+  const [password, handleChangePassword] = useInput(INIT_STATE);
+
   return (
     <>
-      <input data-testid={TEST_ID.INPUT.EMAIL} type="text" />
-      <input data-testid={TEST_ID.INPUT.PASSWORD} type="password" />
+      <Input type="email" value={email} onChange={handleChangeEmail} testId={TEST_ID.INPUT.EMAIL} />
+      <Input type="password" value={password} onChange={handleChangePassword} testId={TEST_ID.INPUT.PASSWORD} />
       <button data-testid={TEST_ID.BUTTON.SIGN_IN}>로그인</button>
     </>
   );
