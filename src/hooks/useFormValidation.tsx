ecc2839f26845e3a6ec4b6cf56f7b0f09f2f56d2
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-10 17:06:20 +0900

    feat: 폼 유효성 검사하는 훅 생성

---
+++ src/hooks/useFormValidation.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import { validateEmail, validatePassword } from "../utils/util";
+
+function useFormValidation({ email, password }: any) {
+  const [isValid, setIsValid] = useState(false);
+  useEffect(
+    function updateIsValidStatus() {
+      const isFormValid = validateEmail(email) && validatePassword(password);
+      setIsValid(isFormValid);
+    },
+    [email, password]
+  );
+
+  return isValid;
+}
+
+export default useFormValidation;
