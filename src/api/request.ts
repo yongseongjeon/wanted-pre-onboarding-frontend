d99b260729e706aa7574bbfe8bc69bf659705fcc
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-11 15:19:19 +0900

    feat: 할 일 목록 fetch 함수 생성

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,12 +1,12 @@
-import { setItemToLocalStorage } from "../utils/localStorage";
-import { SIGN_IN_API_URL, SIGN_UP_API_URL } from "./url";
+import { getItemFromLocalStorage, setItemToLocalStorage } from "../utils/localStorage";
+import { FULL_REQUEST_URL } from "./url";
 
 async function signUp({ email, password }: AuthProps) {
   const headers = {
     "Content-Type": "application/json",
   };
   const body = JSON.stringify({ email, password });
-  const res = await fetch(SIGN_UP_API_URL, { method: "POST", headers, body });
+  const res = await fetch(FULL_REQUEST_URL.SIGN_UP, { method: "POST", headers, body });
   return res;
 }
 
@@ -15,13 +15,23 @@ async function signIn({ email, password }: AuthProps) {
     "Content-Type": "application/json",
   };
   const body = JSON.stringify({ email, password });
-  const res = await fetch(SIGN_IN_API_URL, { method: "POST", headers, body });
+  const res = await fetch(FULL_REQUEST_URL.SIGN_IN, { method: "POST", headers, body });
   const { access_token: receivedAccessToken } = await res.json();
   setItemToLocalStorage("accessToken", receivedAccessToken);
   return res;
 }
 
-export { signUp, signIn };
+async function getTodos() {
+  const storedAccessToken = getItemFromLocalStorage("accessToken");
+  const headers = {
+    Authorization: `Bearer ${storedAccessToken}`,
+  };
+  const res = await fetch(FULL_REQUEST_URL.GET_TODOS, { headers });
+  const resJson = await res.json();
+  return resJson;
+}
+
+export { signUp, signIn, getTodos };
 
 interface AuthProps {
   email: string;
