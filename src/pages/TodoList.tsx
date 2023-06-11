98a51180f417680e20f09be4a0aa0d78ed0e9ae1
yongseongjeon <yongseong.jeon95@gmail.com>
2023-06-11 17:08:39 +0900

    feat: 추가 버튼 클릭하면 할 일 추가

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -3,12 +3,16 @@ import { ROUTE_PATH } from "../route";
 import useRouter from "../hooks/useRouter";
 import { useEffect, useReducer } from "react";
 import Todo from "../components/Todo/Todo";
-import { setTodos, todoReducer } from "../reducer/todoReducer";
-import { getTodos } from "../api/request";
+import { TodoType, addTodo, setTodos, todoReducer } from "../reducer/todoReducer";
+import { requestAddTodo, requestGetTodos } from "../api/request";
+import Input from "../components/Input/Input";
+import useInput from "../hooks/useInput";
+import { TEST_ID } from "../constants/test";
 
 function TodoList() {
   const { routeTo } = useRouter();
   const [todos, dispatch] = useReducer(todoReducer, []);
+  const [todoInputValue, handleTodoInputValue] = useInput("");
 
   useEffect(function redirectSignInPageIfNotSignedIn() {
     const isSignedIn = validateSignIn();
@@ -19,16 +23,25 @@ function TodoList() {
 
   useEffect(() => {
     async function fetchAndSetTodos() {
-      const todos = await getTodos();
+      const todos = await requestGetTodos();
       dispatch(setTodos({ todos }));
     }
     fetchAndSetTodos();
   }, []);
 
+  async function handleClickAddBtn() {
+    const newTodo = await requestAddTodo({ todo: todoInputValue });
+    dispatch(addTodo(newTodo));
+  }
+
   return (
     <div>
-      {todos.map(({ id, content }) => (
-        <Todo id={id} content={content} />
+      <Input type="text" value={todoInputValue} onChange={handleTodoInputValue} testId={TEST_ID.INPUT.NEW_TODO} />
+      <button type="button" onClick={handleClickAddBtn} data-testid={TEST_ID.BUTTON.NEW_TODO_ADD}>
+        추가
+      </button>
+      {todos.map(({ id, todo, isCompleted }: TodoType) => (
+        <Todo id={id} todo={todo} isCompleted={isCompleted} />
       ))}
     </div>
   );
